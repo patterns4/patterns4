d740c0378090815bcb937749c405151e15629117
Turingcop <simon_evertsson@hotmail.com>
2022-01-13 03:58:45 +0100

    local env, dbconfig

--- cykel/db/dbconfig.docker.js
+++ cykel/db/dbconfig.docker.js
@@ -7,13 +7,3 @@ let config = {
 }
 
 export { config };
-
-// let config = {
-//     "host":     "mysql",
-//     "user":     "user",
-//     "password": "password",
-//     "database": "ebike",
-//     "port": "3306"
-// }
-
-// export { config };
