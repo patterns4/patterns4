1808a9f8f523bce89b67ec48eae86cfd3d99264c
Erik <erik@rundberg.ax>
2022-01-14 02:04:50 +0100

    Adding saldo input

--- laravel/resources/views/settings.blade.php
+++ laravel/resources/views/settings.blade.php
@@ -21,7 +21,7 @@
                             Saldo: {{ Auth::user()->saldo }} kr
                         </p>
                     </div>
-                   
+
                     <div>
                         <h1 class="py-3 text-xl font-semibold">Payment method</h1>
                         <div class="flex gap-3">
@@ -47,7 +47,7 @@
                                         @endif
                                         <div>Invoice</div>
                                     </label>
-                                    
+
                                     <label class="labl">
                                         @if (Auth::user()->payment === "Monthly")
                                         <input type="radio" name="payment" value="Monthly" checked/>
@@ -60,8 +60,27 @@
 
                                 <button formaction="settings/update" type="submit" class="block bg-blue-500 my-3 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-auto">Change</button>
                             </form>
-
                         </div>
+                    </div>
+                    <div>
+                        <h1 class="text-xl font-semibold">Add saldo</h1>
+                        <form method="POST" class="w-min">
+                            @csrf <!-- {{ csrf_field() }} -->
+                            <input name="user_id" type="hidden" value="{{ Auth::user()->user_id}}">
+                            <input name="first_name" type="hidden" value="{{ Auth::user()->first_name}}">
+                            <input name="last_name" type="hidden" value="{{ Auth::user()->last_name}}">
+                            <input name="email" type="hidden" value="{{ Auth::user()->email}}">
+                            <input name="birth_year" type="hidden" value="{{ Auth::user()->birth_year}}">
+                            <input name="phone" type="hidden" value="{{ Auth::user()->phone}}">
+                            <input name="saldo" type="hidden" value="{{ Auth::user()->saldo}}">
+
+                            <div class="border">
+                                <input type="number" name="add_saldo" value="0">
+                            </div>
+
+                            <button formaction="settings/saldo" type="submit" class="block bg-blue-500 my-3 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-auto">Add</button>
+                        </form>
+
                     </div>
                 </div>
                  <form method="POST" action="{{ route('logout') }}" class="py-3 my-3">
