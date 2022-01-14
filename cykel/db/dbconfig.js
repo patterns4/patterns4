937fad7e81894e6d1e62bd039cc965c1c177bfe7
jonkul <jonkullberg@gmail.com>
2022-01-14 05:57:13 +0100

    Update dbconfig.js

--- cykel/db/dbconfig.js
+++ cykel/db/dbconfig.js
@@ -1,7 +1,17 @@
+// let config = {
+//     "host":     "localhost",
+//     "user":     "root",
+//     "password": "Bobbyk55",
+//     "database": "ebike",
+//     "port": "3306"
+// }
+
+// export { config };
+
 let config = {
-    "host":     "localhost",
-    "user":     "root",
-    "password": "Bobbyk55",
+    "host":     "mysql",
+    "user":     "user",
+    "password": "password",
     "database": "ebike",
     "port": "3306"
 }
